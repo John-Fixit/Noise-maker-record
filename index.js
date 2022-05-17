8b29e2f0303cd59f103ab32ed7465a21d3332c8f
Adeoye John Oluwakayode <Johnfixit293@gmail.com>
2022-05-17 10:16:30 +0100

    adding of student into the noise maker list, adding the number of times, subtracting form it, and also deleting student was done successfully

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const ejs = require("ejs")
-app.use(bodyParser.urlencoded({extended:true}))
+const ejs = require("ejs");
+const { all } = require("express/lib/application");
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine", "ejs")
+
+let allNoiseMaker = [];
+let times = 1
 app.get("/", (req, res) => {
     // res.sendFile(__dirname + "index.html")
     // res.send("Hello world")
-    res.render("index")
+    res.render("index", { allNoiseMaker, times })
 })
-app.post("/submit",(req,res)=>{
-    res.render("submit")
+app.post("/submit", (req, res) => {
+    res.render("submit", { times })
 })
-
+app.post("/sub", (req, res) => {
+    console.log(req.body);
+    allNoiseMaker.push(req.body)
+    res.redirect("/")
+})
+app.post("/Add", (req, res) => {
+    let myIndex = parseInt(req.body.myIndex);
+    let counTimes = parseInt(allNoiseMaker[myIndex].times);
+    counTimes += 1
+    allNoiseMaker[myIndex].times = counTimes
+    res.redirect("/")
+})
+app.post("/Minus", (req, res) => {
+    let myIndex = parseInt(req.body.myIndex);
+    let counTimes = parseInt(allNoiseMaker[myIndex].times);
+    counTimes -= 1
+    allNoiseMaker[myIndex].times = counTimes
+    if (allNoiseMaker[myIndex].times < 1) {
+        allNoiseMaker[myIndex].times = 0
+    }
+    res.redirect("/")
+})
+app.post("/Clr", (req, res) => {
+    let myIndex = parseInt(req.body.myIndex);
+  let found = allNoiseMaker.filter((students, index)=>(
+      index!=myIndex
+  ))
+    allNoiseMaker = found;
+    res.redirect("/")
+})
 app.listen(3000, () => {
     `My app is listen at port 3000`
 })
